fb55b427cf5e979f85b39ebbdeaa9d0097bcc03f
murat <muratcgunay@gmail.com>
2016-03-15 21:15:37 +0200

    very first commit

---
+++ ConsoleApplication2/ConsoleApplication2/Event.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    public class Event
+    {
+
+        public int priority;
+
+        public Event(int priority)
+        {
+            this.priority = priority;
+        }
+
+    }
+}
